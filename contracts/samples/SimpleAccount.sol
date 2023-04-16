805b99cace9aff7d5d9d9f5df9b3808b163f5d27
Alexander <shchepetov0@gmail.com>
2023-04-16 21:07:09 +0300

    Run prettier-sol

--- contracts/samples/SimpleAccount.sol
+++ contracts/samples/SimpleAccount.sol
@@ -13,12 +13,17 @@ import "../core/BaseAccount.sol";
 import "./callback/TokenCallbackHandler.sol";
 
 /**
-  * minimal account.
-  *  this is sample minimal account.
-  *  has execute, eth handling methods
-  *  has a single signer that can send requests through the entryPoint.
-  */
-contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, Initializable {
+ * minimal account.
+ *  this is sample minimal account.
+ *  has execute, eth handling methods
+ *  has a single signer that can send requests through the entryPoint.
+ */
+contract SimpleAccount is
+    BaseAccount,
+    TokenCallbackHandler,
+    UUPSUpgradeable,
+    Initializable
+{
     using ECDSA for bytes32;
 
     //filler member, to push the nonce and owner to the same slot
@@ -31,7 +36,10 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
 
     IEntryPoint private immutable _entryPoint;
 
-    event SimpleAccountInitialized(IEntryPoint indexed entryPoint, address indexed owner);
+    event SimpleAccountInitialized(
+        IEntryPoint indexed entryPoint,
+        address indexed owner
+    );
 
     modifier onlyOwner() {
         _onlyOwner();
@@ -48,7 +56,6 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
         return _entryPoint;
     }
 
-
     // solhint-disable-next-line no-empty-blocks
     receive() external payable {}
 
@@ -59,13 +66,20 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
 
     function _onlyOwner() internal view {
         //directly from EOA owner, or through the account itself (which gets redirected through execute())
-        require(msg.sender == owner || msg.sender == address(this), "only owner");
+        require(
+            msg.sender == owner || msg.sender == address(this),
+            "only owner"
+        );
     }
 
     /**
      * execute a transaction (called directly from owner, or by entryPoint)
      */
-    function execute(address dest, uint256 value, bytes calldata func) external {
+    function execute(
+        address dest,
+        uint256 value,
+        bytes calldata func
+    ) external {
         _requireFromEntryPointOrOwner();
         _call(dest, value, func);
     }
@@ -73,7 +87,9 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
     /**
      * execute a sequence of transactions
      */
-    function executeBatch(address[] calldata dest, bytes[] calldata func) external {
+    function executeBatch(address[] calldata dest, bytes[] calldata func)
+        external
+    {
         _requireFromEntryPointOrOwner();
         require(dest.length == func.length, "wrong array lengths");
         for (uint256 i = 0; i < dest.length; i++) {
@@ -84,7 +100,7 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
     /**
      * @dev The _entryPoint member is immutable, to reduce gas consumption.  To upgrade EntryPoint,
      * a new implementation of SimpleAccount must be deployed with the new EntryPoint address, then upgrading
-      * the implementation by calling `upgradeTo()`
+     * the implementation by calling `upgradeTo()`
      */
     function initialize(address anOwner) public virtual initializer {
         _initialize(anOwner);
@@ -97,25 +113,37 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
 
     // Require the function call went through EntryPoint or owner
     function _requireFromEntryPointOrOwner() internal view {
-        require(msg.sender == address(entryPoint()) || msg.sender == owner, "account: not Owner or EntryPoint");
+        require(
+            msg.sender == address(entryPoint()) || msg.sender == owner,
+            "account: not Owner or EntryPoint"
+        );
     }
 
     /// implement template method of BaseAccount
-    function _validateAndUpdateNonce(UserOperation calldata userOp) internal override {
+    function _validateAndUpdateNonce(UserOperation calldata userOp)
+        internal
+        override
+    {
         require(_nonce++ == userOp.nonce, "account: invalid nonce");
     }
 
     /// implement template method of BaseAccount
-    function _validateSignature(UserOperation calldata userOp, bytes32 userOpHash)
-    internal override virtual returns (uint256 validationData) {
+    function _validateSignature(
+        UserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal virtual override returns (uint256 validationData) {
         bytes32 hash = userOpHash.toEthSignedMessageHash();
         if (owner != hash.recover(userOp.signature))
             return SIG_VALIDATION_FAILED;
         return 0;
     }
 
-    function _call(address target, uint256 value, bytes memory data) internal {
-        (bool success, bytes memory result) = target.call{value : value}(data);
+    function _call(
+        address target,
+        uint256 value,
+        bytes memory data
+    ) internal {
+        (bool success, bytes memory result) = target.call{value: value}(data);
         if (!success) {
             assembly {
                 revert(add(result, 32), mload(result))
@@ -134,7 +162,7 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
      * deposit more funds for this account in the entryPoint
      */
     function addDeposit() public payable {
-        entryPoint().depositTo{value : msg.value}(address(this));
+        entryPoint().depositTo{value: msg.value}(address(this));
     }
 
     /**
@@ -142,13 +170,19 @@ contract SimpleAccount is BaseAccount, TokenCallbackHandler, UUPSUpgradeable, In
      * @param withdrawAddress target to send to
      * @param amount to withdraw
      */
-    function withdrawDepositTo(address payable withdrawAddress, uint256 amount) public onlyOwner {
+    function withdrawDepositTo(address payable withdrawAddress, uint256 amount)
+        public
+        onlyOwner
+    {
         entryPoint().withdrawTo(withdrawAddress, amount);
     }
 
-    function _authorizeUpgrade(address newImplementation) internal view override {
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        view
+        override
+    {
         (newImplementation);
         _onlyOwner();
     }
 }
-
--- contracts/samples/gnosis/EIP4337Manager.sol
+++ contracts/samples/gnosis/EIP4337Manager.sol
@@ -14,7 +14,7 @@ import "../../interfaces/IAccount.sol";
 import "../../interfaces/IEntryPoint.sol";
 import "../../utils/Exec.sol";
 
-    using ECDSA for bytes32;
+using ECDSA for bytes32;
 
 /**
  * Main EIP4337 module.
@@ -24,13 +24,12 @@ import "../../utils/Exec.sol";
  * Inherits GnosisSafe so that it can reference the memory storage
  */
 contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
-
     address public immutable eip4337Fallback;
     address public immutable entryPoint;
 
     // return value in case of signature failure, with no time-range.
     // equivalent to _packValidationData(true,0,0);
-    uint256 constant internal SIG_VALIDATION_FAILED = 1;
+    uint256 internal constant SIG_VALIDATION_FAILED = 1;
 
     address internal constant SENTINEL_MODULES = address(0x1);
 
@@ -42,9 +41,12 @@ contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
     /**
      * delegate-called (using execFromModule) through the fallback, so "real" msg.sender is attached as last 20 bytes
      */
-    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
-    external override returns (uint256 validationData) {
-        address msgSender = address(bytes20(msg.data[msg.data.length - 20 :]));
+    function validateUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external override returns (uint256 validationData) {
+        address msgSender = address(bytes20(msg.data[msg.data.length - 20:]));
         require(msgSender == entryPoint, "account: not from entrypoint");
 
         GnosisSafe pThis = GnosisSafe(payable(address(this)));
@@ -62,7 +64,9 @@ contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
 
         if (missingAccountFunds > 0) {
             //Note: MAY pay more than the minimum, to deposit for future transactions
-            (bool success,) = payable(msgSender).call{value : missingAccountFunds}("");
+            (bool success, ) = payable(msgSender).call{
+                value: missingAccountFunds
+            }("");
             (success);
             //ignore failure (its EntryPoint's job to verify, not account.)
         }
@@ -81,17 +85,14 @@ contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
         bytes memory data,
         Enum.Operation operation
     ) external {
-        address msgSender = address(bytes20(msg.data[msg.data.length - 20 :]));
+        address msgSender = address(bytes20(msg.data[msg.data.length - 20:]));
         require(msgSender == entryPoint, "account: not from entrypoint");
-        require(msg.sender == eip4337Fallback, "account: not from EIP4337Fallback");
-
-        bool success = execute(
-            to,
-            value,
-            data,
-            operation,
-            type(uint256).max
+        require(
+            msg.sender == eip4337Fallback,
+            "account: not from EIP4337Fallback"
         );
+
+        bool success = execute(to, value, data, operation, type(uint256).max);
 
         bytes memory returnData = Exec.getReturnData(type(uint256).max);
         // Revert with the actual reason string
@@ -105,7 +106,6 @@ contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
         }
     }
 
-
     /**
      * set up a safe as EIP-4337 enabled.
      * called from the GnosisSafeAccountFactory during construction time
@@ -116,8 +116,14 @@ contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
         EIP4337Manager manager //the manager (this contract)
     ) external {
         GnosisSafe safe = GnosisSafe(payable(address(this)));
-        require(!safe.isModuleEnabled(manager.entryPoint()), "setup4337Modules: entrypoint already enabled");
-        require(!safe.isModuleEnabled(manager.eip4337Fallback()), "setup4337Modules: eip4337Fallback already enabled");
+        require(
+            !safe.isModuleEnabled(manager.entryPoint()),
+            "setup4337Modules: entrypoint already enabled"
+        );
+        require(
+            !safe.isModuleEnabled(manager.eip4337Fallback()),
+            "setup4337Modules: eip4337Fallback already enabled"
+        );
         safe.enableModule(manager.entryPoint());
         safe.enableModule(manager.eip4337Fallback());
     }
@@ -129,10 +135,17 @@ contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
      * @param oldManager the old EIP4337 manager to remove, returned by getCurrentEIP4337Manager
      * @param newManager the new EIP4337Manager, usually with a new EntryPoint
      */
-    function replaceEIP4337Manager(address prevModule, EIP4337Manager oldManager, EIP4337Manager newManager) public {
+    function replaceEIP4337Manager(
+        address prevModule,
+        EIP4337Manager oldManager,
+        EIP4337Manager newManager
+    ) public {
         GnosisSafe pThis = GnosisSafe(payable(address(this)));
         address oldFallback = oldManager.eip4337Fallback();
-        require(pThis.isModuleEnabled(oldFallback), "replaceEIP4337Manager: oldManager is not active");
+        require(
+            pThis.isModuleEnabled(oldFallback),
+            "replaceEIP4337Manager: oldManager is not active"
+        );
         pThis.disableModule(oldFallback, oldManager.entryPoint());
         pThis.disableModule(prevModule, oldFallback);
 
@@ -151,40 +164,71 @@ contract EIP4337Manager is IAccount, GnosisSafeStorage, Executor {
      *  we don't test full transaction
      */
     function validateEip4337(GnosisSafe safe, EIP4337Manager manager) public {
-
         // this prevents mistaken replaceEIP4337Manager to disable the module completely.
         // minimal signature that pass "recover"
         bytes memory sig = new bytes(65);
         sig[64] = bytes1(uint8(27));
         sig[2] = bytes1(uint8(1));
         sig[35] = bytes1(uint8(1));
-        UserOperation memory userOp = UserOperation(address(safe), uint256(nonce), "", "", 0, 1000000, 0, 0, 0, "", sig);
+        UserOperation memory userOp = UserOperation(
+            address(safe),
+            uint256(nonce),
+            "",
+            "",
+            0,
+            1000000,
+            0,
+            0,
+            0,
+            "",
+            sig
+        );
         UserOperation[] memory userOps = new UserOperation[](1);
         userOps[0] = userOp;
         IEntryPoint _entryPoint = IEntryPoint(payable(manager.entryPoint()));
         try _entryPoint.handleOps(userOps, payable(msg.sender)) {
             revert("validateEip4337: handleOps must fail");
         } catch (bytes memory error) {
-            if (keccak256(error) != keccak256(abi.encodeWithSignature("FailedOp(uint256,string)", 0, "AA24 signature error"))) {
+            if (
+                keccak256(error) !=
+                keccak256(
+                    abi.encodeWithSignature(
+                        "FailedOp(uint256,string)",
+                        0,
+                        "AA24 signature error"
+                    )
+                )
+            ) {
                 revert(string(error));
             }
         }
     }
+
     /**
      * enumerate modules, and find the currently active EIP4337 manager (and previous module)
      * @return prev prev module, needed by replaceEIP4337Manager
      * @return manager the current active EIP4337Manager
      */
-    function getCurrentEIP4337Manager(GnosisSafe safe) public view returns (address prev, address manager) {
+    function getCurrentEIP4337Manager(GnosisSafe safe)
+        public
+        view
+        returns (address prev, address manager)
+    {
         prev = address(SENTINEL_MODULES);
-        (address[] memory modules,) = safe.getModulesPaginated(SENTINEL_MODULES, 100);
-        for (uint i = 0; i < modules.length; i++) {
+        (address[] memory modules, ) = safe.getModulesPaginated(
+            SENTINEL_MODULES,
+            100
+        );
+        for (uint256 i = 0; i < modules.length; i++) {
             address module = modules[i];
-            try EIP4337Fallback(module).eip4337manager() returns (address _manager) {
+            try EIP4337Fallback(module).eip4337manager() returns (
+                address _manager
+            ) {
                 return (prev, _manager);
+            } catch // solhint-disable-next-line no-empty-blocks
+            {
+
             }
-            // solhint-disable-next-line no-empty-blocks
-            catch {}
             prev = module;
         }
         return (address(0), address(0));
