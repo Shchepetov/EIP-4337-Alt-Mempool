351a59298cf3f31be7eb005d2b8f4e7483d36f9d
Alexander <shchepetov0@gmail.com>
2023-04-17 07:05:11 +0300

    Add test contracts using forbidden opcodes

---
+++ contracts/test/forbidden_opcodes/TestPaymasterGASLIMIT.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.12;
+
+import "../../core/BasePaymaster.sol";
+
+/**
+ * @title Test paymaster with _validatePaymasterUserOp using the 'GASLIMIT' opcode.
+ */
+contract TestPaymasterGASLIMIT is BasePaymaster {
+    constructor(IEntryPoint _entryPoint) BasePaymaster(_entryPoint) {}
+
+    function _validatePaymasterUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 maxCost
+    )
+        internal
+        view
+        virtual
+        override
+        returns (bytes memory context, uint256 validationData)
+    {
+        return ("", block.gaslimit > 0 ? 1 : 0);
+    }
+}
